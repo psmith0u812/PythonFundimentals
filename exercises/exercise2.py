26c7f89de8eedc3aad2e67d996eb202fae6aa491
Philip J. Smith <psmith2@wsutech.edu>
2022-03-01 17:42:15 -0600

    CAC-102 Exercise2

---
+++ exercises/exercise2.py
@@ -0,0 +1,8 @@
+# Write a function called simple() Assign a diffrent message to 5
+# variables and print each message.
+
+def simple():
+    """Agree with user"""
+    print(bookname), 'Was my favorite too!'
+
+bookname = 'Count of Monte Cristo'
